e18ea95192f0b2bd2dd4b8b613a7944dfd160139
Gustavo Fischer <gustavooliveirafischer@hotmail.com>
2022-10-12 00:07:42 -0300

    Definição de player e de quem ira começar jogando

---
+++ socket_client.c
@@ -0,0 +1,163 @@
+
+#include <winsock2.h>
+#include <windows.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+// **************************************************************************
+// definicoes iniciais
+#define TAMANHO_BUFFER 512          // tamanho do buffer de recepcao
+#define DEFAULT_PORT "27015"        // porta de comunicacao
+#define IP_SERVIDOR "127.0.0.1"     // ip do servidor
+// **************************************************************************
+
+int main(int argc, char **argv) 
+{
+    // variaveis
+    WSADATA wsaData;                  // variavel para o winsock 
+    int iResult;                      // variavel de status
+    // sockets para conexao com o servidor
+    SOCKET ConnectSocket = INVALID_SOCKET;    
+    // estrutura de enderecos
+    struct addrinfo *result = NULL,
+                    *ptr = NULL,
+                    hints;    
+    // buffers de recepcao e envio                   
+    char sendbuf[TAMANHO_BUFFER];     
+    char recvbuf[TAMANHO_BUFFER];     
+    int recvbuflen = TAMANHO_BUFFER;  // comprimento do buffer de recepcao    
+
+    // inicializa o winsock
+    iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
+    if (iResult != 0) {
+        printf("WSAStartup falhou com o erro: %d\n", iResult);
+        system("pause");
+        return 1;
+    }
+
+    // pega informacoes da maquina
+    char hostname[NI_MAXHOST];        // guarda nome da maquina
+    gethostname(hostname,NI_MAXHOST); // pega nome da maquina
+
+    // pega o ip da maquina e exibe
+    PHOSTENT phe = gethostbyname(hostname);     
+    int i;
+    for (i = 0; phe->h_addr_list[i] != 0; ++i) {
+        struct in_addr addr;
+        memcpy(&addr, phe->h_addr_list[i], sizeof(struct in_addr));      
+        
+    }
+
+    // inicializa o tipo de conexao
+    ZeroMemory( &hints, sizeof(hints) );
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;    // datagramas TCP
+    hints.ai_protocol = IPPROTO_TCP;
+
+    // resolve o endereco e porta do servidor
+    iResult = getaddrinfo(IP_SERVIDOR, DEFAULT_PORT, &hints, &result);
+    if ( iResult != 0 ) {
+        printf("getaddrinfo falhou com o erro: %d\n", iResult);
+        WSACleanup();
+        system("pause");
+        return 1;
+    }
+
+    // tenta conectar ao endereco do servidor
+    printf("Aguardando outro jogador...");
+    for(ptr=result; ptr != NULL ;ptr=ptr->ai_next) {
+
+        // cria um socket para se conectar ao servidor
+        ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+        if (ConnectSocket == INVALID_SOCKET) {
+            printf("Socket falhou com o erro: %ld\n", WSAGetLastError());
+            WSACleanup();
+            system("pause");
+            return 1;
+        }
+
+        // tenta conecta ao servidor
+        iResult = connect( ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+        if (iResult == SOCKET_ERROR) {
+            closesocket(ConnectSocket);
+            ConnectSocket = INVALID_SOCKET;
+            continue;
+        }
+        break;
+    }
+
+    freeaddrinfo(result);
+
+    if (ConnectSocket == INVALID_SOCKET) {
+        printf("Nao foi possivel se conectar\n");
+        system("pause");
+        WSACleanup();
+        return 1;
+    }    
+
+    // *********************************************************************
+    // Estabeleceu conexao com o servidor e pode trocar dados
+    	char posicao[2];
+    	int player;
+		iResult = recv(ConnectSocket, recvbuf, recvbuflen, 0);
+
+		if(recvbuf[0] == '0'){
+    		printf("\nVoce e o player X\n");
+    		player=0;
+		}
+		else{
+			printf("\nVoce e o player O\n");
+			player=1;
+		}
+
+    while(sendbuf != "0") {
+
+       if(player==0){
+			gets(posicao);
+			sprintf(sendbuf,posicao);
+			iResult = send( ConnectSocket, sendbuf, (int)strlen(sendbuf), 0 );
+			iResult = recv(ConnectSocket, recvbuf, recvbuflen, 0);
+			printf("Mensagem recebida: \"%s\"\n", recvbuf);
+			
+			//guarda na matriz e printa
+			//verifica se ha vitoria, derrota ou empate
+		}
+		
+		else{
+			iResult = recv(ConnectSocket, recvbuf, recvbuflen, 0);
+			printf("Mensagem recebida: \"%s\"\n", recvbuf);
+			gets(posicao);
+			sprintf(sendbuf,posicao);
+			iResult = send( ConnectSocket, sendbuf, (int)strlen(sendbuf), 0 );
+			
+			//guarda na matriz e printa
+			//verifica se ha vitoria, derrota ou empate
+		}
+
+    
+        Sleep(1000);
+
+    }
+
+    // *********************************************************************
+    // Encerra a conexao
+    iResult = shutdown(ConnectSocket, SD_SEND);
+    if (iResult == SOCKET_ERROR) {
+        printf("Falhou encerrar a conexao com o erro: %d\n", WSAGetLastError());
+        closesocket(ConnectSocket);
+        WSACleanup();
+        system("pause");
+        return 1;
+    }
+
+    // cleanup
+    printf("Encerrando socket...");
+    closesocket(ConnectSocket);
+    WSACleanup();
+
+    system("pause");
+    return 0;
+}
---
+++ socket_server.c
@@ -0,0 +1,185 @@
+
+#include <winsock2.h>
+#include <windows.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include<time.h>
+
+// **************************************************************************
+// definicoes iniciais
+#define TAMANHO_BUFFER 2          // tamanho do buffer de recepcao e envio
+#define DEFAULT_PORT "27015"        // porta de comunicacao que sera usada
+// **************************************************************************
+
+int __cdecl main(void) 
+{
+    // variaveis
+    WSADATA wsaData;   // variavel para o winsock                                                        
+    int iResult;       // variavel de status
+    // sockets para conexao com o servidor
+    SOCKET ListenSocket = INVALID_SOCKET;
+    SOCKET ClientSocket = INVALID_SOCKET;
+    // estrutura de enderecos
+    struct addrinfo *result = NULL;
+    struct addrinfo hints;
+    // buffers de recepcao e envio
+    int iSendResult;
+    char sendbuf[TAMANHO_BUFFER];
+    char recvbuf[TAMANHO_BUFFER];
+    int recvbuflen = TAMANHO_BUFFER;     // comprimento do buffer de recepcao  
+      
+
+    // inicializa o winsock
+    iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
+    if (iResult != 0) {
+        printf("WSAStartup falhou com o erro: %d\n", iResult);
+        system("pause");
+        return 1;
+    }
+
+     // pega informacoes da maquina
+    char hostname[NI_MAXHOST];        // guarda nome da maquina
+    gethostname(hostname,NI_MAXHOST); // pega nome da maquina
+
+    // pega o ip da maquina e exibe
+    PHOSTENT phe = gethostbyname(hostname);     
+    int i;
+    for (i = 0; phe->h_addr_list[i] != 0; ++i) {
+        struct in_addr addr;
+        memcpy(&addr, phe->h_addr_list[i], sizeof(struct in_addr));      
+        
+    }
+
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+    hints.ai_flags = AI_PASSIVE;
+
+    // Resolve o endereco do servidor e a porta 
+    iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
+    if ( iResult != 0 ) {
+        printf("getaddrinfo falhou com erro: %d\n", iResult);
+        WSACleanup();
+        system("pause");
+        return 1;
+    }
+
+    // Cria um socket para conexao com o servidor
+    ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+    if (ListenSocket == INVALID_SOCKET) {
+        printf("socket falhou com erro: %ld\n", WSAGetLastError());
+        freeaddrinfo(result);
+        WSACleanup();
+        system("pause");
+        return 1;
+    }
+
+    // Configura o socket TCP do servidor
+    iResult = bind( ListenSocket, result->ai_addr, (int)result->ai_addrlen);
+    if (iResult == SOCKET_ERROR) {
+        printf("bind falhou com erro: %d\n", WSAGetLastError());
+        freeaddrinfo(result);
+        closesocket(ListenSocket);
+        WSACleanup();
+        system("pause");
+        return 1;
+    }
+
+
+    freeaddrinfo(result);
+
+    // Escuta aguardando um cliente 
+    printf("Aguardando outro jogador...", DEFAULT_PORT);
+    iResult = listen(ListenSocket, SOMAXCONN);
+    if (iResult == SOCKET_ERROR) {
+        printf("listen falhou com erro: %d\n", WSAGetLastError());
+        closesocket(ListenSocket);
+        WSACleanup();
+        system("pause");
+        return 1;
+    }
+
+    // Aceita o socket com o cliente
+    ClientSocket = accept(ListenSocket, NULL, NULL);
+    if (ClientSocket == INVALID_SOCKET) {
+        printf("accept falhou com erro: %d\n", WSAGetLastError());
+        closesocket(ListenSocket);
+        WSACleanup();
+        system("pause");
+        return 1;
+    }
+	
+    // No longer need server socket
+    closesocket(ListenSocket);
+    
+    
+		char posicao[2];
+    	printf("\nO jogo vai comecar");
+    	srand(time(NULL));
+    	//define qual é o player 'x' ou 'o' sendo 0=x e 1=o
+    	int player= rand() % 2;
+    	if(player==0){
+    		printf("\nVoce e o player X\n");
+    		sprintf(sendbuf,"1");
+    		iSendResult = send( ClientSocket, sendbuf,(int)strlen(sendbuf), 0 );
+		}
+		else{
+			printf("\nVoce e o player O\n");
+			sprintf(sendbuf,"0");
+    		iSendResult = send( ClientSocket, sendbuf,(int)strlen(sendbuf), 0 );
+		}
+
+
+    // *********************************************************************
+    // Estabeleceu conexao com o cliente e pode trocar dados
+    do {
+    	
+		if(player==0){
+			gets(posicao);
+			sprintf(sendbuf,posicao);
+    		iSendResult = send( ClientSocket, sendbuf,(int)strlen(sendbuf), 0 );
+			iResult = recv(ClientSocket, recvbuf, recvbuflen, 0);
+			printf("Mensagem recebida: \"%s\"\n", recvbuf);
+			
+			//guarda na matriz e printa
+			//verifica se ha vitoria, derrota ou empate
+		}
+		
+		else{
+			iResult = recv(ClientSocket, recvbuf, recvbuflen, 0);
+			printf("Mensagem recebida: \"%s\"\n", recvbuf);
+			gets(posicao);
+			sprintf(sendbuf,posicao);
+    		iSendResult = send( ClientSocket, sendbuf,(int)strlen(sendbuf), 0 );
+			
+			//guarda na matriz e printa
+			//verifica se ha vitoria, derrota ou empate
+		}
+		
+	
+
+    } while (posicao!= "-1");
+    
+
+    // *********************************************************************
+    // Encerra a conexao
+    iResult = shutdown(ClientSocket, SD_SEND);
+    printf("\nJogo encerrado");
+    if (iResult == SOCKET_ERROR) {
+        printf("shutdown failed with error: %d\n", WSAGetLastError());
+        closesocket(ClientSocket);
+        WSACleanup();
+        system("pause");
+        return 1;
+    }
+
+    // cleanup
+    printf("Encerrando socket...");
+    closesocket(ClientSocket);
+    WSACleanup();
+
+    system("pause");
+    return 0;
+}
